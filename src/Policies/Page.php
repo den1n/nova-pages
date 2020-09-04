c4e55e4d01dfe0a2825c3be76f86ba393f49bee2
Dmitry Kadochnikov <iqmass@gmail.com>
2020-09-04 16:04:11 +0500

    Removed debug code.

--- src/Policies/Page.php
+++ src/Policies/Page.php
@@ -11,43 +11,36 @@ class Page
 
     public function viewAny($user): bool
     {
-        error_log(__METHOD__);
         return true;
     }
 
     public function view($user, Model $page): bool
     {
-        error_log(__METHOD__);
         return $user->id == $page->author_id or $page->is_published;
     }
 
     public function create($user): bool
     {
-        error_log(__METHOD__);
         return true;
     }
 
     public function update($user, Model $page): bool
     {
-        error_log(__METHOD__);
         return $user->id == $page->author_id;
     }
 
     public function delete($user, Model $page): bool
     {
-        error_log(__METHOD__);
         return $user->id == $page->author_id;
     }
 
     public function restore($user, Model $page): bool
     {
-        error_log(__METHOD__);
         return $user->id == $page->author_id;
     }
 
     public function forceDelete($user, Model $page): bool
     {
-        error_log(__METHOD__);
         return $user->id == $page->author_id;
     }
 }
